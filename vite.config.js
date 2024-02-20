5c70e8ad3974c13047ea73036f8454f380be1716
Ddevaraj <ddevaraj@synamedia.com>
2024-02-21 02:03:27 +0530

    added dependencies

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://ddevaraj14.github.io/memory_game/",
+  base: "/memory_game/",
 });
