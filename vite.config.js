469eb9d25eb965992d2220f339edf56fa2346f83
Ddevaraj <ddevaraj@synamedia.com>
2024-02-21 01:38:42 +0530

    added base URL

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "https://ddevaraj14.github.io/memory_game/",
+});
